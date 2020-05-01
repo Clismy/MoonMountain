17bab164c8d7c753820d1a709a4bd56fccafb639
Franz <a17frajo@student.his.se>
2020-05-01 20:07:07 +0200

    Bytte namn på projekt mappen
    var tvungen att ta bort mellanslaget i namnet på projekt mapen för att få git ignoren att fungera korekt

---
+++ MoonMountain/Assets/_Scripts/PlayerJump.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+
+    bool desiredJump = false;
+
+    Rigidbody rb;
+    PlayerGround pG;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pG = GetComponent<PlayerGround>();
+    }
+
+    void Update()
+    {
+        desiredJump |= Input.GetButtonDown("Jump");
+    }
+
+    void FixedUpdate()
+    {
+        if (desiredJump)
+        {
+            Jump();
+            desiredJump = false;
+        }
+    }
+
+    void Jump()
+    {
+        if (pG.IsGrounded())
+        {
+            float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+            rb.velocity += Vector3.up * jumpSpeed;
+        }
+    }
+
+    public void AddJump()
+    {
+        float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+        rb.velocity += Vector3.up * jumpSpeed * 2f;
+    }
+}
