f10e6e13a1d17d240fe58f33757883e6a6b968f5
Franz <a17frajo@student.his.se>
2020-05-01 17:45:17 +0200

    Adderade individuella arbetsmappar
    Adderade mappar som vi kan sitta och jobba i utan att störa varandra.

---
+++ Moon Mountain/Assets/__Working_Files/_Franz/DUMMY_FÖR_ATT_GIT_SKA_TA_MED_MAPPEN2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DUMMY_FÖR_ATT_GIT_SKA_TA_MED_MAPPEN2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
